e4c53a4ed6c8db4659cab9ea9686aadeac7ba096
Lukas <lukas.vaic@qest.cz>
2022-05-29 10:21:03 +0200

    Update FieldProps.tsx

--- src/FieldProps.tsx
+++ src/FieldProps.tsx
@@ -1,5 +1,8 @@
+import { ReactNode } from "react";
+
 export interface FormikFieldProps {
   name: string
   validate?: (value: any) => undefined | string | Promise<any>
   fast?: boolean
+  children?: ReactNode
 }
